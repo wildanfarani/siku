6ed04c094f7dd66cb3bcd82a37d018a2a731be5e
wildanfarani <wildanfrni@gmail.com>
2022-06-20 21:19:53 +0700

    first commit

---
+++ src/main/java/id/go/kemenkeu/siku/SikuApplication.java
@@ -0,0 +1,13 @@
+package id.go.kemenkeu.siku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SikuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SikuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/id/go/kemenkeu/siku/SikuApplicationTests.java
@@ -0,0 +1,13 @@
+package id.go.kemenkeu.siku;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SikuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
